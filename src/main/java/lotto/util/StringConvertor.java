ef852f3a3f7913b2e4818a3ee0d8d854d3654ca8
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-13 13:29:29 +0900

    feat : 쉼표로 구분된 문자열을 List<Integer> 타입으로 변환하는 기능 추가

--- src/main/java/lotto/util/StringConvertor.java
+++ src/main/java/lotto/util/StringConvertor.java
@@ -1,5 +1,10 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.view.validator.Separator;
+
 public class StringConvertor {
 
     private StringConvertor() {
@@ -7,5 +12,11 @@ public class StringConvertor {
 
     public static int convertStringToInt(String value) {
         return Integer.parseInt(value);
+    }
+
+    public static List<Integer> convertCommaSeparatedStringToIntegerList(String value) {
+        return Arrays.stream(Separator.COMMA.split(value))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
